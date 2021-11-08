a2905b78fb778f370834eda46dd98d870723dc25
Laura Cole <lauracole1900@comcast.net>
2021-11-08 10:19:13 -0700

    updated package.json, started building on App.js and birthday component

---
+++ src/components/birthday.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+const Birthday = (props) => {
+  return (
+    <Container>
+      <Row>
+        <Col sm={3}>
+
+        </Col>
+      </Row>
+    </Container>
+  )
+}
