298e83a7dd1a955f75d351564dc4f7bf21f8ffd2
Laura Cole <lauracole1900@comcast.net>
2021-11-08 13:19:02 -0700

    working on confetti render, not working yet

--- src/components/birthday.js
+++ src/components/birthday.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Confetti from "react-confetti";
+import Typed from "typed.js";
 
 const Birthday = (props) => {
+  let quoteArr = [];
+  const currentText = useRef("");
+  const [confettiReady, setConfettiReady] = useState(false);
+
+  props.quotes.forEach(quote => {
+    quoteArr = [...quoteArr, quote.quote]
+    return quoteArr
+  });
+  console.log({ quoteArr });
+
+
+  useEffect(() => {
+    const options = {
+      strings: quoteArr,
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: false,
+      showCursor: true
+    }
+    console.log(options.strings);
+    const typed = new Typed("#texts", options);
+
+    
+    if (currentText.current.innerHTML === quoteArr[quoteArr.length - 1]) {
+      setConfettiReady(true);
+    }
+     
+    return () => {
+      typed.destroy();
+    }
+    
+  }, [confettiReady])
+
+
   return (
     <Container>
       <Row>
-        <Col sm={3}>
-
+        <Col sm={3} className="center">
+          <section id="texts" ref={currentText}></section>
+          {confettiReady === true &&
+            <Confetti />}
         </Col>
       </Row>
     </Container>
   )
 }
+
+export default Birthday;
